a31a2a7d8ad395103de6f1bf7d8a211d8835806c
M. Lance Hilliard <lance@digitalcreations.cc>
2014-10-08 15:42:20 -0500

    startOutlookAfterDelay: Office 2013 compat

--- StartupScripts/startOutlookAfterDelay.bat
+++ StartupScripts/startOutlookAfterDelay.bat
@@ -1,5 +1,5 @@
 @echo off
 title Will launch Outlook...
 timeout /t 10 /NOBREAK
-start "" "C:\Program Files (x86)\Microsoft Office\Office14\OUTLOOK.EXE"
+start "" "C:\Program Files (x86)\Microsoft Office\Office15\OUTLOOK.EXE"
 
