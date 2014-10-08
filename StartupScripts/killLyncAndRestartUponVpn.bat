8caa981ab6dbed58e7e1a9ccd7a854c7429939bf
M. Lance Hilliard <lance@digitalcreations.cc>
2014-10-08 15:43:16 -0500

    killLyncAndRestartUponVpn: Office 2013 compat

--- StartupScripts/killLyncAndRestartUponVpn.bat
+++ StartupScripts/killLyncAndRestartUponVpn.bat
@@ -7,10 +7,10 @@ if %errorlevel%==0 goto end
 title Terminating Lync...
 :terminateLync
 timeout 1 >nul 2>&1
-taskkill /IM communicator.exe >nul 2>&1
+taskkill /IM lync.exe /F >nul 2>&1
 if not %errorlevel%==0 goto terminateLync
 timeout 3 >nul 2>&1
-tasklist /FI "IMAGENAME eq communicator.exe" | findstr communicator.exe >nul 2>&1
+tasklist /FI "IMAGENAME eq lync.exe" | findstr lync.exe >nul 2>&1
 if %errorlevel%==0 goto terminateLync
 
 title Will launch Lync (waiting for VPN connection)...
@@ -18,6 +18,6 @@ title Will launch Lync (waiting for VPN connection)...
 ping -n 2 %AUTOVPN_REMOTE_LAN_IP% | find "TTL"
 if %errorlevel%==1 goto detectVpnConnection
 
-start "" "C:\Program Files (x86)\Microsoft Lync\communicator.exe"
+start "" "C:\Program Files (x86)\Microsoft Office\Office15\lync.exe" /fromrunkey
 
 :end
