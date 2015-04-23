4dea938da4850184435a78d6b672412760aeedaf
M. Lance Hilliard <lance@digitalcreations.cc>
2015-04-23 15:12:02 -0500

    add user script: GV: Hide Hangouts Ad

---
+++ UserScripts/GoogleVoiceHideHangoutsAd.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         GV: Hide Hangouts Ad
+// @namespace    http://digitalcreations.cc
+// @version      0.1
+// @description  Hide the Hangouts ad which appears on the Voice webpage
+// @author       You
+// @match        https://www.google.com/voice
+// @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
+// ==/UserScript==
+
+if (jQuery) {
+    jQuery(document).ready(function () {
+        function hideAd() {
+            var ads = $('div.jfk-bubble');
+            if (ads.length > 0) {
+                ads.hide();
+            }
+            else {
+                setTimeout(hideAd, 1000);
+            }
+        }
+        hideAd();
+    });
+}
