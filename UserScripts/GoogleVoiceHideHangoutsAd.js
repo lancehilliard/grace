f7a593994b0c017262061b73a1ac1e6fdbaf3884
M. Lance Hilliard <lance@digitalcreations.cc>
2015-04-23 15:21:02 -0500

    change version format of GV userscript

--- UserScripts/GoogleVoiceHideHangoutsAd.js
+++ UserScripts/GoogleVoiceHideHangoutsAd.js
@@ -4,7 +4,7 @@
 // @description  Hide the Hangouts ad which appears on the Voice webpage
 // @author       @lancehilliard
 // @downloadUrl  https://raw.githubusercontent.com/lancehilliard/grace/master/UserScripts/GoogleVoiceHideHangoutsAd.js
-// @version      1.1
+// @version      0.201504231520
 // @match        https://www.google.com/voice
 // @grant        none
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
